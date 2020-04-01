42f69648287af0d014f01178a0a1be3e812ed83b
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-01 17:23:59 -0400

    chore: add new db

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 console.log(chalk.yellow('Opening database connection'));
 
-const database = 'my_boiler_plate';
+const database = 'my_koa_boiler_plate';
 const databaseURL = process.env.DATABASE_URL || `postgres://localhost:5432/${database}`;
 const db = new Sequelize(databaseURL, { 
   logging: false
