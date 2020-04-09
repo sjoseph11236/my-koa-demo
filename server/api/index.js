1055fe23c56d8eeec6525a4062ef8c312c74d122
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-08 23:51:33 -0400

    chore: add solution for no db

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ const router =  new Router({
   prefix: '/api/users'
 });
 
+
 router.param('id', async(id, ctx, next) => {
   console.log('here')
   ctx.state.user = await User.findByPk(id);
@@ -12,8 +13,8 @@ router.param('id', async(id, ctx, next) => {
 })
 
 router.get('/', async (ctx, next )=> { 
-  const user = await User.findAll();
-  ctx.body = user; 
+  const users = await User.findAll();
+  ctx.body = users; 
 })
 
 
@@ -44,5 +45,53 @@ router.delete('/:id', async (ctx, next) => {
   const user = await User.destroy({where: { id }});
   ctx.body = {};
 });
+
+/* IF NEEDED */
+
+// let users = [
+//   {
+//     "name": "Sayeed",
+//     "email": "sayeed@email.com"
+//   },
+//   {
+//     "name": "Samantha",
+//     "email": "Samantha@email.com"
+//   }
+// ]
+
+
+// router.get('/', async(ctx, next) => { 
+//   ctx.body = users;
+// });
+
+// router.post('/', async(ctx, next) => { 
+//   const user = ctx.request.body
+//   users.push(user);
+//   ctx.body = user;
+// });
+
+// router.put('/:id', async(ctx, next) => { 
+//   const id = ctx.params.id;
+//   const body = ctx.request.body;
+//   users = users.map( (user, idx) => {
+//     if(idx + 1 == id) {
+//       user = {...user,...body };
+//     }
+//     return user;
+//   });
+
+//   ctx.body = users;
+// });
+
+// router.delete('/:id', async(ctx, next) => { 
+//   const id = ctx.params.id;
+//   users = users.filter( (user, idx) => {
+//     if((idx + 1) !== id) { 
+//       return user;
+//     }
+//   })
+//   ctx.body = {};
+// });
+
 
 module.exports = router;
