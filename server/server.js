620f6bd70a165a4e8eb01bd56a26e32e1d854000
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-08 22:57:18 -0400

    chore: remove files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const Koa = require('koa');
-const json = require('koa-json');
 const bodyParser = require('koa-bodyparser');
 const logger = require('koa-logger');
 const static = require('koa-static');
@@ -15,6 +14,7 @@ const apiRouter = require('./api');
 // Logger Middleware
 app.use(logger());
 
+// Custom Logger Middleware
 app.use(async(ctx, next) =>  { 
   let start = new Date();
   await next();
@@ -24,51 +24,8 @@ app.use(async(ctx, next) =>  {
 });
 
 
-app.use(async (ctx, next) => { 
-  ctx.state.message = await message();
-  ctx.body = ctx.state.message;
-})
-
-const message = () => {
-  return 'Hello World';
-}
-
-
-
-// JSON Prettier Middleware
-app.use(json());
 // Body Parser Middleware
 app.use(bodyParser());
-// // Static middlware
-// app.use(static('./public'));
-
-
-// Express Static Middlleware
-// app.use(express.static('public'))
-
-// // Router Middleware
-app.use(apiRouter.routes())
-
-// // Starter hello world 
-// *** Cascading example ***
-
-// app.use(async (ctx, next) => {
-//   console.log('here')
-//   await next();
-//   console.log('finally')
-// })
-
-// app.use(async (ctx, next) => { 
-//   console.log('here too');
-//   ctx.body = 'Hello World';
-// });
-
-// Express Middleware
-// app.use(( req, res, next) => { 
-//   res.send("Hello")
-// })
-
-
 
 // Error Middleware
 app.use(async (ctx, next) => {
@@ -81,11 +38,22 @@ app.use(async (ctx, next) => {
 });
 
 
-// Express Error Middleware
-// app.use((error,req, res, next) => { 
-//   console.error(err.stack)
-//   res.status(500).send('Something broke!')
+// app.use(async (ctx, next) => { 
+//   ctx.state.message =  message();
+//   ctx.body = ctx.state.message;
+//   next();
 // })
+
+// const message = () => {
+//   return 'Hello World';
+// }
+
+
+// Static middlware
+app.use(static('./public'));
+
+// // Router Middleware
+app.use(apiRouter.routes())
 
 // // // Start the app..
 app.listen(PORT, ()=> console.log(`Server is listening on PORT ${PORT}`));
